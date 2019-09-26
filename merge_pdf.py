407375c49c96e24d761d5305dd02f6ffd6456008
JorgeDeLosSantos <delossantosmfq@gmail.com>
2019-09-26 14:32:02 -0500

    up

--- merge_pdf.py
+++ merge_pdf.py
@@ -6,9 +6,13 @@ def merger(output_path, input_paths):
     pdf_writer = PdfFileWriter()
  
     for path in input_paths:
-        pdf_reader = PdfFileReader(path)
-        for page in range(pdf_reader.getNumPages()):
-            pdf_writer.addPage(pdf_reader.getPage(page))
+        try:
+            pdf_reader = PdfFileReader(path)
+            for page in range(pdf_reader.getNumPages()):
+                pdf_writer.addPage(pdf_reader.getPage(page))
+        except:
+            pass
+        
  
     with open(output_path, 'wb') as fh:
         pdf_writer.write(fh)
